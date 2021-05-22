5784f3bdca292db6145676af3e8977da5ee3c7f2
Nemtullo <koznem24@gmail.com>
2021-05-22 11:22:44 +0200

    Interesting number formattin in c

---
+++ Oct_Hex_Decimal values/Printing_different_number_formats.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    int value = 16;
+
+    printf("Value in Hex format : %x\n", value);
+    printf("Value in Dec format : %d\n", value);
+    printf("Value in Oct format : %o\n", value);
+}
